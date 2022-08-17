6839bdf3224f35523b7bc24f5d0c62beee722d9c
xhsf <827032783@qq.com>
2022-08-18 00:04:05 +0800

    测试代码

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jiaxwu/goleveldb
 
 go 1.18
 
-require github.com/syndtr/goleveldb v1.0.0 // indirect
+require (
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
 
 func main() {
-	fmt.Println("abc")
+	db, err := leveldb.OpenFile("path/to/db", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	data, err := db.Get([]byte("key"), nil)
+	log.Println(err)
+	log.Println(data)
+	err = db.Put([]byte("key"), []byte("value"), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err = db.Get([]byte("key"), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(data)
+	err = db.Delete([]byte("key"), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err = db.Get([]byte("key"), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(err)
 }
